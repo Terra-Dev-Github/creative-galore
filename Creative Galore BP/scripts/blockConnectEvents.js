ace0315efc8efeae2321c2ba8737255ed6a618bb
HendersonHens <gamesheavenly7@gmail.com>
2025-01-10 17:28:08 -0600

    beta 02 fix

--- Creative Galore BP/scripts/blockConnectEvents.js
+++ Creative Galore BP/scripts/blockConnectEvents.js
@@ -12,30 +12,7 @@ import { world } from '@minecraft/server';
 world.beforeEvents.worldInitialize.subscribe(eventData => {
     // register a custom component named terra:side_self_connect for blocks that only connect to themselves in a horizontal plane
     eventData.blockComponentRegistry.registerCustomComponent('terra:side_self_connect', {
-        onPlace: (e) => {
-            // destructure event data for easier access
-            const { block } = e;
-
-            // get adjacent blocks
-            const north = block.north();
-            const east = block.east();
-            const south = block.south();
-            const west = block.west();
-
-            // update block states based on the presence of adjacent blocks that match the one placed
-            if (north?.typeId === block.typeId) block.setPermutation(block.permutation.withState('terra:south_connect', north ? 1 : 0));
-            else block.setPermutation(block.permutation.withState('terra:south_connect', 0));
-
-            if (south?.typeId === block.typeId) block.setPermutation(block.permutation.withState('terra:north_connect', south ? 1 : 0));
-            else block.setPermutation(block.permutation.withState('terra:north_connect', 0));
-
-            if (east?.typeId === block.typeId) block.setPermutation(block.permutation.withState('terra:west_connect', east ? 1 : 0));
-            else block.setPermutation(block.permutation.withState('terra:west_connect', 0));
-
-            if (west?.typeId === block.typeId) block.setPermutation(block.permutation.withState('terra:east_connect', west ? 1 : 0));
-            else block.setPermutation(block.permutation.withState('terra:east_connect', 0));
-        },
-        onPlayerDestroy: (e) => {
+        onTick: (e) => {
             // destructure event data for easier access
             const { block } = e;
 
@@ -61,61 +38,7 @@ world.beforeEvents.worldInitialize.subscribe(eventData => {
     });
     // register a custom component named terra:double_connect for block pairs that connect in a horizontal plane, based on their direction
     eventData.blockComponentRegistry.registerCustomComponent('terra:double_connect', {
-        onPlace: (e) => {
-            // destructure event data for easier access
-            const { block } = e;
-
-            // get adjacent blocks, their direction and states
-            const north = block.north();
-            const east = block.east();
-            const south = block.south();
-            const west = block.west();
-            const cardinalDirection = block?.permutation.getState('minecraft:cardinal_direction');
-
-            const northCD = north?.permutation.getState('minecraft:cardinal_direction');
-            const southCD = south?.permutation.getState('minecraft:cardinal_direction');
-            const eastCD = east?.permutation.getState('minecraft:cardinal_direction');
-            const westCD = west?.permutation.getState('minecraft:cardinal_direction');
-
-            // update block states based on the surrounding blocks and their cardinal direction
-            if (cardinalDirection === 'north') {
-                if (east?.typeId === block.typeId && eastCD === 'north' && !east.hasTag('terra:is_pair_connected')) {
-                    block.setPermutation(block.permutation.withState('terra:block_left', east ? 1 : 0));
-                } else block.setPermutation(block.permutation.withState('terra:block_left', 0));
-
-                if (west?.typeId === block.typeId && westCD === 'north' && !west.hasTag('terra:is_pair_connected')) {
-                    block.setPermutation(block.permutation.withState('terra:block_right', west ? 1 : 0));
-                } else block.setPermutation(block.permutation.withState('terra:block_right', 0));
-            };
-            if (cardinalDirection === 'south') {
-                if (east?.typeId === block.typeId && eastCD === 'south' && !east.hasTag('terra:is_pair_connected')) {
-                    block.setPermutation(block.permutation.withState('terra:block_right', east ? 1 : 0));
-                } else block.setPermutation(block.permutation.withState('terra:block_right', 0));
-
-                if (west?.typeId === block.typeId && westCD === 'south' && !west.hasTag('terra:is_pair_connected')) {
-                    block.setPermutation(block.permutation.withState('terra:block_left', west ? 1 : 0));
-                } else block.setPermutation(block.permutation.withState('terra:block_left', 0));
-            };
-            if (cardinalDirection === 'east') {
-                if (north?.typeId === block.typeId && northCD === 'east' && !north.hasTag('terra:is_pair_connected')) {
-                    block.setPermutation(block.permutation.withState('terra:block_right', north ? 1 : 0));
-                } else block.setPermutation(block.permutation.withState('terra:block_right', 0));
-
-                if (south?.typeId === block.typeId && southCD === 'east' && !south.hasTag('terra:is_pair_connected')) {
-                    block.setPermutation(block.permutation.withState('terra:block_left', south ? 1 : 0));
-                } else block.setPermutation(block.permutation.withState('terra:block_left', 0));
-            };
-            if (cardinalDirection === 'west') {
-                if (north?.typeId === block.typeId && northCD === 'west' && !north.hasTag('terra:is_pair_connected')) {
-                    block.setPermutation(block.permutation.withState('terra:block_left', north ? 1 : 0));
-                } else block.setPermutation(block.permutation.withState('terra:block_left', 0));
-
-                if (south?.typeId === block.typeId && southCD === 'west' && !south.hasTag('terra:is_pair_connected')) {
-                    block.setPermutation(block.permutation.withState('terra:block_right', south ? 1 : 0));
-                } else block.setPermutation(block.permutation.withState('terra:block_right', 0));
-            };
-        },
-        onPlayerDestroy: (e) => {
+        onTick: (e) => {
             // destructure event data for easier access
             const { block } = e;
 
@@ -172,61 +95,7 @@ world.beforeEvents.worldInitialize.subscribe(eventData => {
     });
     // register a custom component named terra:lateral_connect for multiple blocks that connect in a horizontal plane, based on their direction
     eventData.blockComponentRegistry.registerCustomComponent('terra:lateral_connect', {
-        onPlace: (e) => {
-            // destructure event data for easier access
-            const { block } = e;
-
-            // get adjacent blocks, their direction and states
-            const north = block.north();
-            const east = block.east();
-            const south = block.south();
-            const west = block.west();
-            const cardinalDirection = block?.permutation.getState('minecraft:cardinal_direction');
-
-            const northCD = north?.permutation.getState('minecraft:cardinal_direction');
-            const southCD = south?.permutation.getState('minecraft:cardinal_direction');
-            const eastCD = east?.permutation.getState('minecraft:cardinal_direction');
-            const westCD = west?.permutation.getState('minecraft:cardinal_direction');
-
-            // update block states based on the surrounding blocks and their cardinal direction
-            if (cardinalDirection === 'north') {
-                if (east?.typeId === block.typeId && eastCD === 'north') {
-                    block.setPermutation(block.permutation.withState('terra:block_left', east ? 1 : 0));
-                } else block.setPermutation(block.permutation.withState('terra:block_left', 0));
-
-                if (west?.typeId === block.typeId && westCD === 'north') {
-                    block.setPermutation(block.permutation.withState('terra:block_right', west ? 1 : 0));
-                } else block.setPermutation(block.permutation.withState('terra:block_right', 0));
-            };
-            if (cardinalDirection === 'south') {
-                if (east?.typeId === block.typeId && eastCD === 'south') {
-                    block.setPermutation(block.permutation.withState('terra:block_right', east ? 1 : 0));
-                } else block.setPermutation(block.permutation.withState('terra:block_right', 0));
-
-                if (west?.typeId === block.typeId && westCD === 'south') {
-                    block.setPermutation(block.permutation.withState('terra:block_left', west ? 1 : 0));
-                } else block.setPermutation(block.permutation.withState('terra:block_left', 0));
-            };
-            if (cardinalDirection === 'east') {
-                if (north?.typeId === block.typeId && northCD === 'east') {
-                    block.setPermutation(block.permutation.withState('terra:block_right', north ? 1 : 0));
-                } else block.setPermutation(block.permutation.withState('terra:block_right', 0));
-
-                if (south?.typeId === block.typeId && southCD === 'east') {
-                    block.setPermutation(block.permutation.withState('terra:block_left', south ? 1 : 0));
-                } else block.setPermutation(block.permutation.withState('terra:block_left', 0));
-            };
-            if (cardinalDirection === 'west') {
-                if (north?.typeId === block.typeId && northCD === 'west') {
-                    block.setPermutation(block.permutation.withState('terra:block_left', north ? 1 : 0));
-                } else block.setPermutation(block.permutation.withState('terra:block_left', 0));
-
-                if (south?.typeId === block.typeId && southCD === 'west') {
-                    block.setPermutation(block.permutation.withState('terra:block_right', south ? 1 : 0));
-                } else block.setPermutation(block.permutation.withState('terra:block_right', 0));
-            };
-        },
-        onPlayerDestroy: (e) => {
+        onTick: (e) => {
             // destructure event data for easier access
             const { block } = e;
 
@@ -283,101 +152,7 @@ world.beforeEvents.worldInitialize.subscribe(eventData => {
     });
     // register a custom component named 'terra:wall_connect' for wall blocks that only connect in a horizontal plane
     eventData.blockComponentRegistry.registerCustomComponent('terra:wall_connect', {
-        onPlace: (e) => {
-            // destructure event data for easier access
-            const { block } = e;
-            block.permutation.withState('terra:upper_pieces', 0)
-
-            // get adjacent blocks
-            const aboveBlock = block.above();
-            const north = block.north();
-            const east = block.east();
-            const south = block.south();
-            const west = block.west();
-
-            // define an array of block types to exclude from connections
-            const excludeBlocksArray = [
-                'minecraft:air',
-                'minecraft:snow_layer',
-                'minecraft:wooden_door',
-                'minecraft:iron_door',
-                'minecraft:acacia_door',
-                'minecraft:birch_door',
-                'minecraft:crimson_door',
-                'minecraft:dark_oak_door',
-                'minecraft:jungle_door',
-                'minecraft:oak_door',
-                'minecraft:spruce_door',
-                'minecraft:warped_door',
-                'minecraft:mangrove_door',
-                'minecraft:cherry_door',
-                'minecraft:bamboo_door',
-                'minecraft:iron_trapdoor',
-                'minecraft:acacia_trapdoor',
-                'minecraft:birch_trapdoor',
-                'minecraft:crimson_trapdoor',
-                'minecraft:dark_oak_trapdoor',
-                'minecraft:jungle_trapdoor',
-                'minecraft:oak_trapdoor',
-                'minecraft:spruce_trapdoor',
-                'minecraft:warped_trapdoor',
-                'minecraft:mangrove_trapdoor',
-                'minecraft:cherry_trapdoor',
-                'minecraft:bamboo_trapdoor',
-                'minecraft:trapdoor',
-                // custom excluded blocks from other add-ons
-                'terra:poplar_trapdoor',
-                'terra:lab_trapdoor'
-                // add other block types you want to exclude
-            ];
-
-            // check if the adjacent block is not in the excludeBlocksArray
-            const northConnects = !excludeBlocksArray.includes(north?.typeId);
-            const eastConnects = !excludeBlocksArray.includes(east?.typeId);
-            const southConnects = !excludeBlocksArray.includes(south?.typeId);
-            const westConnects = !excludeBlocksArray.includes(west?.typeId);
-
-            // define an array of blocks that aren't completely solid, but can change their states and therefore the type of connection above
-            const conditionedBlocksArray = [
-                'minecraft:fence',
-                'minecraft:wall',
-                // custom conditioned blocks from other add-ons
-                'terra:andesite_brick_wall',
-                'terra:bone_brick_wall',
-                'terra:diorite_brick_wall',
-                'terra:dripstone_brick_wall',
-                'terra:granite_brick_wall',
-                'terra:mixed_stonebrick_wall',
-                'terra:polished_andesite_brick_wall',
-                'terra:polished_bone_brick_wall',
-                'terra:polished_diorite_brick_wall',
-                'terra:polished_dripstone_brick_wall',
-                'terra:polished_granite_brick_wall',
-                'terra:polished_limestone_brick_wall',
-                'terra:polished_schist_brick_wall',
-                'terra:schist_brick_wall'
-            ];
-
-            // check if the above block has certain conditions based on these arrays
-            const fullAboveBlock = !conditionedBlocksArray.includes(aboveBlock?.typeId);
-
-            // update block states based on the presence of adjacent blocks
-            block.setPermutation(block.permutation.withState('terra:north', northConnects ? 1 : 0));
-            block.setPermutation(block.permutation.withState('terra:south', southConnects ? 1 : 0));
-            block.setPermutation(block.permutation.withState('terra:east', eastConnects ? 1 : 0));
-            block.setPermutation(block.permutation.withState('terra:west', westConnects ? 1 : 0));
-
-            // if above block isn't conditioned, toggle upper pieces
-            if (aboveBlock.typeId !== 'minecraft:air' && !aboveBlock.hasTag('terra:conditioned_hollow')) {
-                block.setPermutation(block.permutation.withState('terra:upper_pieces', fullAboveBlock ? 1 : 0));
-            } else block.setPermutation(block.permutation.withState('terra:upper_pieces', 0));
-
-            // if above block is conditioned and has upper pieces, toggle the former
-            if (aboveBlock.typeId !== 'minecraft:air' && aboveBlock.hasTag('terra:conditioned_hollow')) {
-                block.setPermutation(block.permutation.withState('terra:upper_pieces', aboveBlock.permutation.withState('terra:upper_pieces', 0) ? 1 : 0));
-            } else block.setPermutation(block.permutation.withState('terra:upper_pieces', 0));
-        },
-        onPlayerDestroy: (e) => {
+        onTick: (e) => {
             // destructure event data for easier access
             const { block } = e;
             block.permutation.withState('terra:upper_pieces', 0)
--- Creative Galore BP/scripts/curtainProperties.js
+++ Creative Galore BP/scripts/curtainProperties.js
@@ -12,72 +12,7 @@ import { world } from '@minecraft/server';
 world.beforeEvents.worldInitialize.subscribe(eventData => {
     // register a custom component named 'terra:curtain_properties' for the block behavior (vertical connecting based on cardinal direction)
     eventData.blockComponentRegistry.registerCustomComponent('terra:curtain_properties', {
-        onPlace: (e) => {
-            // destructure event data for easier access
-            const { block } = e;
-
-            // get blocks around our block and their direction
-            const aboveBlock = block.above();
-            const belowBlock = block.below();
-            const cardinalDirection = block?.permutation.getState('minecraft:cardinal_direction');
-            const aboveCardinalDirection = aboveBlock?.permutation.getState('minecraft:cardinal_direction');
-            const belowCardinalDirection = belowBlock?.permutation.getState('minecraft:cardinal_direction');
-
-            // define an array of blocks to include in vertical connecting (i.e. all curtains)
-            const includeBlocks = [
-                'terra:black_curtain',
-                'terra:blue_curtain',
-                'terra:brown_curtain',
-                'terra:cyan_curtain',
-                'terra:gray_curtain',
-                'terra:green_curtain',
-                'terra:light_blue_curtain',
-                'terra:light_gray_curtain',
-                'terra:lime_curtain',
-                'terra:magenta_curtain',
-                'terra:orange_curtain',
-                'terra:pink_curtain',
-                'terra:purple_curtain',
-                'terra:red_curtain',
-                'terra:white_curtain',
-                'terra:yellow_curtain'
-            ];
-
-            // check if the adjacent block is in the array
-            const aboveConnects = includeBlocks.includes(aboveBlock?.typeId);
-            const belowConnects = includeBlocks.includes(belowBlock?.typeId);
-
-            // update block states based on the surrounding blocks and their cardinal direction
-            if (cardinalDirection === 'north') {
-                if (aboveCardinalDirection === 'north') block.setPermutation(block.permutation.withState('terra:curtain_bottom', aboveConnects ? 1 : 0));
-                else block.setPermutation(block.permutation.withState('terra:curtain_bottom', 0));
-
-                if (belowCardinalDirection === 'north') block.setPermutation(block.permutation.withState('terra:curtain_top', belowConnects ? 1 : 0));
-                else block.setPermutation(block.permutation.withState('terra:curtain_top', 0));
-            };
-            if (cardinalDirection === 'south') {
-                if (aboveCardinalDirection === 'south') block.setPermutation(block.permutation.withState('terra:curtain_bottom', aboveConnects ? 1 : 0));
-                else block.setPermutation(block.permutation.withState('terra:curtain_bottom', 0));
-
-                if (belowCardinalDirection === 'south') block.setPermutation(block.permutation.withState('terra:curtain_top', belowConnects ? 1 : 0));
-                else block.setPermutation(block.permutation.withState('terra:curtain_top', 0));
-            };
-            if (cardinalDirection === 'east') {
-                if (aboveCardinalDirection === 'east') block.setPermutation(block.permutation.withState('terra:curtain_bottom', aboveConnects ? 1 : 0));
-                else block.setPermutation(block.permutation.withState('terra:curtain_bottom', 0));
-
-                if (belowCardinalDirection === 'east') block.setPermutation(block.permutation.withState('terra:curtain_top', belowConnects ? 1 : 0));
-                else block.setPermutation(block.permutation.withState('terra:curtain_top', 0));
-            };
-            if (cardinalDirection === 'west') {
-                if (aboveCardinalDirection === 'west') block.setPermutation(block.permutation.withState('terra:curtain_bottom', aboveConnects ? 1 : 0));
-                else block.setPermutation(block.permutation.withState('terra:curtain_bottom', 0));
-
-                if (belowCardinalDirection === 'west') block.setPermutation(block.permutation.withState('terra:curtain_top', belowConnects ? 1 : 0));
-                else block.setPermutation(block.permutation.withState('terra:curtain_top', 0));
-            };
-        },
-        onPlayerDestroy: (e) => {
+        onTick: (e) => {
             // destructure event data for easier access
             const { block } = e;
 
